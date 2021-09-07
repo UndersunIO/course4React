3f6e3bedde18bee452460aee04afaacf0af00530
UndersunIO <alex.atc.digital@gmail.com>
2021-09-07 21:17:54 +0200

    header/footer component + bootstrap social et font awesome

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
 import Menu from './MenuComponent';
+import Header from './HeaderComponent';
+import Footer from './FooterComponent';
 import DishDetail from './DishdetailComponent.js'; 
 import { DISHES } from '../shared/dishes';
+
+
 
 class Main extends Component {
 
@@ -20,14 +23,11 @@ class Main extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <div>
+        <Header />
         <Menu dishes={this.state.dishes} onClick={(dishId) => this.onDishSelect(dishId)} />
         <DishDetail dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} />
+        <Footer/>
       </div>
     );
   }
