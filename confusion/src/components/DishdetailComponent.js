72beaa4c462fd0db0c564301659cb981ec1684d8
UndersunIO <alex.atc.digital@gmail.com>
2021-09-07 19:43:25 +0200

    Push class/function debug

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,8 +2,12 @@ import React, {Component} from 'react';
 import {Card, CardImg, CardBody, CardText, CardTitle} from 'reactstrap';
 
 class DishDetail extends Component {
-
-
+    componentDidMount() {
+        console.log("Dishdetail componentDidMount invoked");
+    }
+    componentDidUpdate() {
+        console.log("Dishdetail componentDidUpdate invoked");
+    }
     renderDish(dish) {
         return(
             <Card>
@@ -26,7 +30,7 @@ class DishDetail extends Component {
             return (
                 <li>
                     <p>{comment.comment}</p>
-                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', {year: 'numeric', month: 'short', day: 'numeric'}).format(new Date(Date(comment.date)))}</p>
                     <p></p>
                 </li>
             );
@@ -42,17 +46,18 @@ class DishDetail extends Component {
     }
 
     render() {
+        console.log("Dishdetail render invoked");
         if (this.props.dish != null) {
             return (
                 <div class="container">
-                <div className="row">
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderDish(this.props.dish)}
+                    <div className="row">
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderDish(this.props.dish)}
+                        </div>
+                        <div className="col-12 col-md-5 m-1">
+                            {this.renderComments(this.props.dish.comments)}
+                        </div>
                     </div>
-                    <div className="col-12 col-md-5 m-1">
-                        {this.renderComments(this.props.dish.comments)}
-                    </div>
-                </div>
                 </div>
             );
         }
--- confusion/src/components/MenuComponent.js
+++ confusion/src/components/MenuComponent.js
@@ -1,11 +1,51 @@
-import React, {Component} from "react";
+import React from "react";
 import {Card, CardImg, CardImgOverlay, CardTitle} from 'reactstrap';
 
 
-class Menu extends Component {
+function RenderMenuItem ({dish, onClick}) {
+    return (
+        <Card
+            onClick={() => onClick(dish.id)}>
+            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImgOverlay>
+                <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+        </Card>
+    );
+}
+
+const Menu = (props) => {
+
+    const menu = props.dishes.map((dish) => {
+        return (
+            <div className="col-12 col-md-5 m-1"  key={dish.id}>
+                <RenderMenuItem dish={dish} onClick={props.onClick} />
+            </div>
+        );
+    });
+
+    return (
+        <div className="container">
+            <div className="row">
+                {menu}
+            </div>
+        </div>
+    );
+}
+
+export default Menu;
+
+/** 
+ *   
+ */
+
+ /** Component class + debug 
+  * 
+  * class Menu extends Component {
 
     constructor(props) {
         super(props);
+        console.log("Menu component constructor invoked");
 
         this.state = {
             selectedDish: null
@@ -14,13 +54,14 @@ class Menu extends Component {
     }
 
     componentDidMount() {
-        
+        console.log("Menu componentDidMount invoked");
     }
 
     onDishSelect(dish) {
         this.setState({selectedDish: dish});
     }
     render() {
+        console.log('Menu Component render invoked');
         const menu = this.props.dishes.map((dish) => {
             return (
                 <div key={dish.id} className="col-12 col-md-5 m-1">
@@ -34,7 +75,6 @@ class Menu extends Component {
             );
         });
 
-        console.log('Menu Component');
 
         return (
             <div className='container'>
@@ -47,3 +87,5 @@ class Menu extends Component {
 }
 
 export default Menu;
+  * 
+  */
