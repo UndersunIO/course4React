7175fec44e6422eea1b204faac530a0d4343e7b4
UndersunIO <alex.atc.digital@gmail.com>
2021-09-07 19:55:13 +0200

    Push class/function debug 100%

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -1,14 +1,15 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card, CardImg, CardBody, CardText, CardTitle} from 'reactstrap';
 
-class DishDetail extends Component {
+
+    /* lifecycle mount and update 
     componentDidMount() {
         console.log("Dishdetail componentDidMount invoked");
     }
     componentDidUpdate() {
         console.log("Dishdetail componentDidUpdate invoked");
-    }
-    renderDish(dish) {
+    }*/
+    function RenderDish({dish}) {
         return(
             <Card>
                 <CardImg top src={dish.image} alt={dish.name} />
@@ -20,7 +21,7 @@ class DishDetail extends Component {
         );
     }
 
-    renderComments(comments) {
+     function RenderComments({comments}) {
         if (comments == null || comments.length === 0) {
             return (
                 <div></div>
@@ -45,17 +46,17 @@ class DishDetail extends Component {
         );
     }
 
-    render() {
+    const DishDetail = (props) => {
         console.log("Dishdetail render invoked");
-        if (this.props.dish != null) {
+        if (props.dish != null) {
             return (
                 <div class="container">
                     <div className="row">
                         <div className="col-12 col-md-5 m-1">
-                            {this.renderDish(this.props.dish)}
+                           <RenderDish dish={props.dish}/>
                         </div>
                         <div className="col-12 col-md-5 m-1">
-                            {this.renderComments(this.props.dish.comments)}
+                            <RenderComments comments={props.dish.comments}/>
                         </div>
                     </div>
                 </div>
@@ -67,7 +68,7 @@ class DishDetail extends Component {
             );
         }
     }
-}
+
 
 
 
