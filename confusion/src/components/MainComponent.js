0aeedf26e410da05c9f643acb1025f71a810c667
UndersunIO <alex.atc.digital@gmail.com>
2021-09-09 01:43:37 +0200

    Week2 100%

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
 import Menu from './MenuComponent';
 import Contact from './ContactComponent';
 import DishDetail from './DishdetailComponent';
 import Header from './HeaderComponent';
-import Footer from './FooterComponent'
+import Footer from './FooterComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
@@ -50,6 +51,7 @@ class Main extends Component {
                 <Route path="/home" component={HomePage} /> 
                 <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes} />}  />
                 <Route exact path='/contactus' component={Contact} />
+                <Route exact path='/aboutus' component={()=><About leaders={this.state.leaders}/>} />
                 <Route path='/menu/:dishId' component={DishWithId} />
             
             
