86133c7ee7d67cec3b7c4a534f88500082768138
UndersunIO <alex.atc.digital@gmail.com>
2021-09-10 22:45:00 +0200

    redux intro

--- confusion/src/components/MainComponent.js
+++ confusion/src/components/MainComponent.js
@@ -10,10 +10,18 @@ import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { LEADERS } from '../shared/leaders';
 import { PROMOTIONS } from '../shared/promotions';
-import { Switch, Route, Redirect } from 'react-router-dom';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
-
+const mapStateToProps = state => {
+  return {
+    dishes: state.dishes,
+    comments: state.comments,
+    promotions: state.promotions,
+    leaders: state.leaders
+  }
+}
 
 class Main extends Component {
 
@@ -27,13 +35,7 @@ class Main extends Component {
     };
   }
   render() {
-    const DishWithId = ({match}) => {
-        return(
-            <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
-              comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
-        );
-      };
-      const HomePage = () => {
+    const HomePage = () => {
           return(
               <Home
                 dish={this.state.dishes.filter((dish) => dish.featured)[0]}
@@ -43,6 +45,13 @@ class Main extends Component {
             
           )
    }
+
+    const DishWithId = ({match}) => {
+      return(
+        <DishDetail dish={this.props.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={this.props.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    );
+  };
 
     return (
         <div>
@@ -64,4 +73,4 @@ class Main extends Component {
   }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
