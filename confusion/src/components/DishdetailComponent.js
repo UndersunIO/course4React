c903b5837bb6b394d954434cbfadbc703614ebbc
UndersunIO <alex.atc.digital@gmail.com>
2021-09-24 00:41:57 +0200

    redux thunk isLoading is ok

--- confusion/src/components/DishdetailComponent.js
+++ confusion/src/components/DishdetailComponent.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import {Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Row, Col, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Loading } from './LoadingComponent';
+
 
     /* lifecycle mount and update 
     componentDidMount() {
@@ -143,7 +145,25 @@ import { Control, LocalForm, Errors } from 'react-redux-form';
     }
     
     const DishDetail = (props) => {
-        if (props.dish != null)
+        if (props.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <Loading />
+                    </div>
+                </div>
+            );
+        }
+        else if (props.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">            
+                        <h4>{props.errMess}</h4>
+                    </div>
+                </div>
+            );
+        }
+        else if (props.dish != null) 
         return (
             <div className="container">
                 <div className="row">
